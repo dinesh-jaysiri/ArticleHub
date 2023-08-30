828859f496463377cce6b7ebf68d480eecb13776
Dinesh Jayasiri <ywdinesh95@gmail.com>
2023-08-30 09:37:54 +0530

    Added UserController

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::get('/users', function(){
+
+    return new JsonResponse([
+        'data'=>['name'=>'dinesh', 'age'=>27]
+
+    ]);
+
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
