650febfa9c11cd98fdfaceab9509a5c6e15171f2
Dinesh Jayasiri <ywdinesh95@gmail.com>
2023-09-01 18:11:42 +0530

    complite Artilce Resources

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,26 +2,41 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreArticleRequest;
-use App\Http\Requests\UpdateArticleRequest;
+use App\Http\Resources\ArticleResource;
 use App\Models\Article;
+use App\Repositories\ArticleRepository;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ArticleController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+
+        $pageSize = $request->page_size ?? 20;
+        $articles = Article::query()->paginate($pageSize);
+
+        return ArticleResource::collection($articles);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreArticleRequest $request)
+    public function store(Request $request, ArticleRepository $repository)
     {
-        //
+        $payload = $request->only(['title', 'body']);
+
+        Validator::validate($payload, [
+            'title' => 'string|max:255',
+            'body' => 'array'
+        ]);
+
+        $created = $repository->create($payload);
+        return new ArticleResource($created);
     }
 
     /**
@@ -29,22 +44,35 @@ class ArticleController extends Controller
      */
     public function show(Article $article)
     {
-        //
+
+        return new ArticleResource($article);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateArticleRequest $request, Article $article)
+    public function update(Request $request, Article $article, ArticleRepository $repository)
     {
-        //
+        $payload = $request->only(['title', 'body']);
+
+        Validator::validate($payload, [
+            'title' => 'string|max:255',
+            'body' => 'array'
+        ]);
+
+        $updated = $repository->update($article, $payload);
+        return new ArticleResource($updated);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Article $article)
+    public function destroy(Article $article,ArticleRepository $repository)
     {
-        //
+        $article = $repository->forceDelete($article);
+        return new JsonResponse([
+            'data' => 'success'
+        ]);
+
     }
 }
--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,14 +4,40 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Article extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['title', 'body', 'user_id'];
     protected $casts = [
         'body' => 'array'
     ];
+
+    public function setTitleAttribute($value)
+    {
+        if (empty($this->attributes['slug'])) {
+            $slug = $this->generateUniqueSlug($value);
+            $this->attributes['slug'] = $slug;
+        }
+
+        $this->attributes['title'] = $value;
+    }
+
+    private function generateUniqueSlug($value)
+    {
+        $originalSlug = Str::slug($value);
+        $slug = $originalSlug;
+        $count = 1;
+
+        while (static::where('slug', $slug)->exists()) {
+            $slug = $originalSlug . '-' . $count++;
+        }
+
+        return $slug;
+    }
+
 
     public function author()
     {
@@ -23,11 +49,13 @@ class Article extends Model
         return $this->hasMany(Comment::class, 'article_id');
     }
 
-    public function likes(){
-        return $this->belongsToMany(User::class,'article_like', 'article_id','user_id');
+    public function likes()
+    {
+        return $this->belongsToMany(User::class, 'article_like', 'article_id', 'user_id');
     }
 
-    public function topics() {
-        return $this->belongsToMany(Topic::class, 'article_topic', 'article_id','topic_id');
+    public function topics()
+    {
+        return $this->belongsToMany(Topic::class, 'article_topic', 'article_id', 'topic_id');
     }
 }
